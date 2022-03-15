80a2d3a874c0b6fc35fe60d87f91dae84aeb17d6
deniswachira <deniswachira77@gmail.com>
2022-03-15 04:15:07 +0300

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+  }
 }
 
 module.exports = nextConfig
