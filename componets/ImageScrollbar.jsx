efa9d9d9a2f9fa541f4c2109ce39abd551ce0297
deniswachira <deniswachira77@gmail.com>
2022-03-17 22:21:50 +0300

    fixed links

--- componets/ImageScrollbar.jsx
+++ componets/ImageScrollbar.jsx
@@ -1,5 +1,5 @@
-import { useContext } from "react/";
-import Image from "next/dist/client/image";
+import { useContext } from "react";
+import Image from "next/image";
 import {Box, Icon, Flex} from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext} from "react-horizontal-scrolling-menu";
 import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa';
