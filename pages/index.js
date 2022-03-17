a65e25b03e5932d691ca84cb741b4f90d4b8728a
deniswachira <deniswachira77@gmail.com>
2022-03-17 11:43:55 +0300

    fixed explore button

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({propertiesForSale, propertiesForRent}) {
           desc1=' Explore from Apartments, builder floors, villas'
           desc2='and more'
           buttonText='Explore Renting'
-          linkName='/search?purpose=for-rent'
+          linkName='/Search?purpose=for-rent'
           imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
           />
       <Flex flexWrap='wrap'>
@@ -41,7 +41,7 @@ export default function Home({propertiesForSale, propertiesForRent}) {
           desc1=' Explore from Apartments, land, builder floors,'
           desc2=' villas and more'
           buttonText='Explore Buying'
-          linkName='/search?purpose=for-sale'
+          linkName='/Search?purpose=for-sale'
           imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
           />
             <Flex flexWrap='wrap'>
